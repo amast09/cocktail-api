a182e3b450839527ade8a0d320deb8041065d32e
Aaron Mast <amast09@gmail.com>
2023-03-04 16:28:16 -0500

    Update Negroni recipe and add API route for determining potential cocktails

--- src/main/scala/CocktailApiRoutes.scala
+++ src/main/scala/CocktailApiRoutes.scala
@@ -9,18 +9,35 @@ import io.circe.syntax._
 import io.circe.generic.auto._
 import org.http4s._
 import cats.effect.IO
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.syntax._
+
+import org.http4s._
+import org.http4s.circe._
+import org.http4s.dsl.io._
 
 object CocktailApiRoutes {
+
+  case class PotentialCocktailsJsonPayload(ingredients: List[Ingredient])
+  implicit val decoder = jsonOf[IO, PotentialCocktailsJsonPayload]
 
   def cocktailRoutes(cocktailService: CocktailService): HttpRoutes[IO] = {
     val dsl = new Http4sDsl[IO] {}
     import dsl._
 
-    HttpRoutes.of[IO] { case GET -> Root / "ingredients" =>
-      for {
-        ingredients <- cocktailService.getIngredients()
-        resp        <- Ok(ingredients.asJson)
-      } yield resp
+    HttpRoutes.of[IO] {
+      case GET -> Root / "ingredients" =>
+        for {
+          ingredients <- cocktailService.getIngredients()
+          resp        <- Ok(ingredients.asJson)
+        } yield resp
+      case req @ POST -> Root / "potential-cocktails" =>
+        for {
+          decodedPayload     <- req.as[PotentialCocktailsJsonPayload]
+          potentialCocktails <- cocktailService.getPotentialCocktails(decodedPayload.ingredients)
+          resp               <- Ok(potentialCocktails.asJson)
+        } yield resp
     }
   }
 }
